68648467363aedebed755eac6e425f34929cdd0f
chirag-parmar <chiragparmar12209@gmail.com>
2024-07-28 13:33:57 +0530

    adding navigating keys

--- firmware/code.py
+++ firmware/code.py
@@ -38,15 +38,21 @@ split = Split(
 keyboard.modules.append(split)
 
 TD_LSFT = KC.TD(KC.LSFT, KC.CW, tap_time=200)
-TD_MO_1 = KC.TD(KC.MO(1), KC.LM(1, KC.LSFT), KC.LM(1, KC.LGUI), tap_time=200)
-TD_MO_2 = KC.TD(KC.MO(2), KC.LM(2, KC.LSFT), KC.LM(2, KC.LGUI), tap_time=200)
+TD_MO_1 = KC.TD(KC.MO(1), KC.LM(1, KC.LSFT), tap_time=200)
+TD_MO_2 = KC.TD(KC.MO(2), KC.LM(2, KC.LSFT), tap_time=200)
 
 combos.combos = [
-    Chord((26, 54), KC.ENT, match_coord=True),
+    # Chord((26, 54), KC.ENT, match_coord=True),
     # Chord((KC.F1, KC.F2, KC.LCTL, KC.RCTL), KC.TG(3)),
     # Sequence((KC.F18, KC.F19, KC.F20), KC.MW_UP, timeout=1000),
     # Sequence((KC.F21, KC.F22, KC.F23), KC.MW_DN, timeout=1000)
 ]
+
+
+# define vim macros
+VI_QUIT = KC.MACRO(":q")
+VI_WRQT = KC.MACRO(":wq")
+VI_SAVE = KC.MACRO(":w")
 
 # GENERAL RULES FOR CONVENIENCE
 #   do not define combos with keys from both sides, will require both hands. rather if combos are defined on one side you can one hands free
@@ -56,22 +62,20 @@ combos.combos = [
 #   it also makes a lot of sense to replace basic modifiers with layer modifiers but keep the count of modifiers the same. like del usually requires fn + backspace but it can be kept as MO(1) + backspace. replacing modifiers only makes sense for modifiers that cannot be accomodated on the keyboard or modifiers that do not require to be accomodated because their frequency of usage is too less.
 #   some keys just dont' make sense on a normal keyboard like capslock. enable tapdance on the lsft key for locking caps.
 #   defining combos on different sides doesn't make a lot of sense  but I would still try to maintain the split for commonly occuring shortcuts. so that if you shift back to normal key board it is easier. example:  indentation shortcut
-#   generally one extra layer is enough to extend the 40% to 100% but that is not the point of custom keyboards. I usually keep an extra layer for quick controls (mouse, media, vim shortcuts)
-#   the best use of string substitution us vim. I cannot stress on this enough but forget typing a colon everytime you wanna navigate through vim controls. my layer 2 is filled with this.
-#   any more layers defined must be for really specific usecases and should not be momentary layer. either you activate and stay on that layer or deactivate and come back. no switching back and forth. example: gaming  
+#   lalyers more than 2 must be defined for really specific usecases and should not be momentary layer. either you activate and stay on that layer or deactivate and come back. no switching back and forth. example: gaming  
 #   The best use case of a custom designed keyboard is to make it more comfortable on your fingers, move the shift key to the thumn - no more stretching pinkies.
 
 keyboard.keymap = [
     [
         KC.ESC,     KC.Q,   KC.W,   KC.E,       KC.R,       KC.T,                               KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,       KC.BSPC,
         KC.TAB,     KC.A,   KC.S,   KC.D,       KC.F,       KC.G,                               KC.H,       KC.J,       KC.K,       KC.L,       KC.SCLN,    KC.QUOT,
-        TD_MO_2,    KC.Z,   KC.X,   KC.C,       KC.V,       KC.B,       KC.MINS,      KC.EQL,   KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.BSLS,
-                                    KC.TRNS,    KC.LSFT,    KC.SPC,     TD_MO_1,      KC.LGUI,  KC.ENT,     KC.LCTL,    KC.RALT
+        KC.LCTL,    KC.Z,   KC.X,   KC.C,       KC.V,       KC.B,       KC.MINS,      KC.EQL,   KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,    KC.BSLS,
+                                    KC.TRNS,    KC.LSFT,    KC.SPC,     TD_MO_1,      KC.LGUI,  KC.ENT,     TD_MO_2,    KC.RALT
     ],
     [
         KC.GRV,   KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,                              KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.DEL,
-        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,                            KC.LPRN,  KC.RPRN,  KC.LBRC,  KC.RBRC,  KC.TRNS,  KC.TRNS,
-        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,        KC.TRNS,  KC.RCBR,  KC.LCBR,  KC.LABK,  KC.RABK,  KC.TRNS,  KC.TRNS,
+        KC.TRNS,  KC.TRNS,  KC.LBRC,  KC.RBRC,  KC.LPRN,  KC.RPRN,                            KC.VOLU,  KC.DOWN,  KC.UP,    KC.RGHT,  KC.MNXT,  KC.MPLY,
+        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.LCBR,  KC.RCBR,  KC.TRNS,        KC.TRNS,  KC.VOLD,  KC.TRNS,  KC.TRNS,  KC.LEFT,  KC.MPRV,  KC.TRNS,
                                       KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS,        KC.TRNS,  KC.TRNS,  KC.TRNS,  KC.TRNS
     ],
     [
